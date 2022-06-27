fd90a8378a28a250972902b59322651282975fef
Erick Casanova <desneruda@gmail.com>
2022-06-27 17:16:26 -0500

    add web services

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use actix_web::{get, web, App, HttpServer, HttpResponse, Responder};
+
 #[macro_use]
 extern crate diesel;
 
@@ -10,22 +12,44 @@ use std::env;
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
 
-fn main() {
+use diesel::r2d2::{self, ConnectionManager};
+use diesel::r2d2::Pool;
+
+#[get("/hello")]
+async fn hello_world() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
-    let conn = PgConnection::establish(&db_url)
-        .expect(&format!("Error connecting to {}", db_url));
 
-    use self::models::{Post, NewPost};
-    use self::schema::posts;
-    use self::schema::posts::dsl::*;
+    let connection = ConnectionManager::<PgConnection>::new(db_url);
+
+    let pool = Pool::builder()
+        .build(connection)
+        .expect("Failed to create pool.");
+
+    
+
+    HttpServer::new(move || {
+        App::new().service(hello_world).data(pool.clone())
+    }).bind(("0.0.0.0", 9900))?.run().await
+
+    // let conn = PgConnection::establish(&db_url)
+    //     .expect(&format!("Error connecting to {}", db_url));
+
+    // use self::models::{Post, NewPost};
+    // use self::schema::posts;
+    // use self::schema::posts::dsl::*;
 
     // post a new post to the database
     // let new_post = NewPost {
-    //     title: "Using Diesel 2",
-    //     slug: "using-diesel2",
+    //     title: "Using Diesel 3",
+    //     slug: "using-diesel3",
     //     body: "This is a blog post using Diesel"
     // };
 
@@ -41,16 +65,16 @@ fn main() {
     //     .expect("Error updating post");
 
     // delete a post from the database
-    diesel::delete(posts.find(3)).execute(&conn).expect("Error deleting post");
+    // diesel::delete(posts.find(3)).execute(&conn).expect("Error deleting post");
 
     // delete all posts with the same slug structure from the database
-    diesel::delete(posts.filter(slug.like("%-diesel%"))).execute(&conn).expect("Error deleting post");
+    // diesel::delete(posts.filter(slug.like("%-diesel%"))).execute(&conn).expect("Error deleting post");
 
 
     // get all posts from the database
-    let posts_result = posts.load::<Post>(&conn)
-        .expect("Error loading posts")
-        .iter()
-        .for_each(|post| println!("{} {} {} {}", post.id, post.title, post.slug, post.body));
+    // let posts_result = posts.load::<Post>(&conn)
+    //     .expect("Error loading posts")
+    //     .iter()
+    //     .for_each(|post| println!("{} {} {} {}", post.id, post.title, post.slug, post.body));
 
 }
